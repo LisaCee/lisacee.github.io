6f98568adb83450a3eae2069faad8a805e057a94
LisaCee <lisaceedesign@gmail.com>
2019-09-08 10:23:21 -0700

    Updates

---
+++ precache-manifest.79bae6ed6a7f5726c35578257554cd25.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b5bfd492dc2c476e8d1d8998599ea5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4be905e9d678f112a83b",
+    "url": "/static/css/main.874267a1.chunk.css"
+  },
+  {
+    "revision": "1c5acfe0f75d1abe75d6",
+    "url": "/static/js/2.54257b7b.chunk.js"
+  },
+  {
+    "revision": "4be905e9d678f112a83b",
+    "url": "/static/js/main.81ddde62.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  },
+  {
+    "revision": "81ffda1644f004a48e1c98d9032832fb",
+    "url": "/static/media/LisaCee_Logo_blue.81ffda16.jpg"
+  },
+  {
+    "revision": "3a03679ae1c0248278d51e0b9b225e24",
+    "url": "/static/media/book-finder.3a03679a.gif"
+  },
+  {
+    "revision": "b5766c483f40de2445505ad8fb3e25c8",
+    "url": "/static/media/knowledge-rocket.b5766c48.gif"
+  },
+  {
+    "revision": "d81ff2f8a57712d5673cc8a746a57c2a",
+    "url": "/static/media/lambda-notes.d81ff2f8.gif"
+  },
+  {
+    "revision": "219ad5fd184d79d33b9f65f834c285b4",
+    "url": "/static/media/movieTimeMachine.219ad5fd.gif"
+  }
+]);
