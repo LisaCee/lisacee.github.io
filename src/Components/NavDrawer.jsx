e0741b6830b6280e68d581cd64af7fdd328f5182
LisaCee <lisaceedesign@gmail.com>
2019-10-01 18:41:04 -0700

    added resume link and social, needs styling

--- src/Components/NavDrawer.jsx
+++ src/Components/NavDrawer.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Hidden from '@material-ui/core/Hidden';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import PersonIcon from '@material-ui/icons/Person';
-import MailIcon from '@material-ui/icons/Mail';
-import ImportantDevices from '@material-ui/icons/ImportantDevices';
-import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
-import '../styles/navDrawer.css';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Hidden from "@material-ui/core/Hidden";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import PersonIcon from "@material-ui/icons/Person";
+import MailIcon from "@material-ui/icons/Mail";
+import ImportantDevices from "@material-ui/icons/ImportantDevices";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import "../styles/navDrawer.css";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex'
+    display: "flex"
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
     })
   },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen
     }),
@@ -46,7 +47,7 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1
   },
   hide: {
-    display: 'none'
+    display: "none"
   },
   drawer: {
     width: drawerWidth,
@@ -56,23 +57,23 @@ const useStyles = makeStyles(theme => ({
     width: drawerWidth
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-start'
+    justifyContent: "flex-start"
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
     }),
     marginRight: -drawerWidth
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen
     }),
@@ -97,27 +98,27 @@ export default function PersistentDrawerRight() {
     <div className={classes.root}>
       <CssBaseline />
       <AppBar
-        position='fixed'
+        position="fixed"
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open
         })}
       >
         <Toolbar>
-          <Typography variant='h5' noWrap className={classes.title} id='name'>
+          <Typography variant="h5" noWrap className={classes.title} id="name">
             Lisa Canini
           </Typography>
           <Hidden xsDown>
-            <Typography variant='body1'>Full Stack Web Developer</Typography>
+            <Typography variant="body1">Full Stack Web Developer</Typography>
           </Hidden>
           <IconButton
-            color='inherit'
-            aria-label='open drawer'
-            edge='end'
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
             onClick={handleDrawerOpen}
             className={clsx(open && classes.hide)}
-            id='navButton'
+            id="navButton"
           >
-            <MenuIcon id='hamburger' />
+            <MenuIcon id="hamburger" />
           </IconButton>
         </Toolbar>
       </AppBar>
@@ -130,8 +131,8 @@ export default function PersistentDrawerRight() {
       </main>
       <Drawer
         className={classes.drawer}
-        variant='persistent'
-        anchor='right'
+        variant="persistent"
+        anchor="right"
         open={open}
         classes={{
           paper: classes.drawerPaper
@@ -139,84 +140,103 @@ export default function PersistentDrawerRight() {
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? (
+            {theme.direction === "rtl" ? (
               <ChevronLeftIcon />
             ) : (
-              <ChevronRightIcon />
-            )}
+                <ChevronRightIcon />
+              )}
           </IconButton>
         </div>
         <Divider />
-        <List className='navList'>
-          <a href='#intro'>
+        <List className="navList">
+          <a href="#intro">
             <ListItem button>
               <ListItemIcon>
                 <PersonIcon />
               </ListItemIcon>
-              <ListItemText primary='About' />
+              <ListItemText primary="About" />
             </ListItem>
           </a>
-          <a href='#gallery'>
+          <a href="#gallery">
             <ListItem button>
               <ListItemIcon>
                 <PhotoLibraryIcon />
               </ListItemIcon>
-              <ListItemText primary='Gallery' />
+              <ListItemText primary="Gallery" />
             </ListItem>
           </a>
-          <a href='#skills'>
+          <a href="#skills">
             <ListItem button>
               <ListItemIcon>
                 <ImportantDevices />
               </ListItemIcon>
-              <ListItemText primary='Skills' />
+              <ListItemText primary="Skills" />
             </ListItem>
           </a>
-          <a href='#contact'>
+          <a href="https://docs.google.com/document/d/1AMnusKqwQAVXko7qqk0BJz-QBk8X-PUtbLSxhsDbPQ8/edit?usp=sharing" target="_"
+            rel="noopener noreferrer">
+            <ListItem button>
+              <ListItemIcon>
+                <FileCopyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Resume" />
+            </ListItem>
+          </a>
+          <a href="#contact">
             <ListItem button>
               <ListItemIcon>
                 <MailIcon />
               </ListItemIcon>
-              <ListItemText primary='Contact' />
+              <ListItemText primary="Contact" />
             </ListItem>
           </a>
-          {/* <Divider />
-          <div className='socialNav'>
-            <ul>
-              <a
-                href='https://www.github.com/lisacee'
-                target='_'
-                rel='noopener noreferrer'
-              >
-                <li className='fab fa-github'>
-                  <span className='icon'>Github</span>
-                </li>
-              </a>
-              <a
-                href='https://www.linkedin.com/in/lisacee'
-                target='_'
-                rel='noopener noreferrer'
-              >
-                <li className='fab fa-linkedin-in'>
-                  <span className='icon'>LinkedIn</span>
-                </li>
-              </a>
-              <a
-                href='https://www.twitter.com/lisacee14'
-                target='_'
-                rel='noopener noreferrer'
-              >
-                <li className='fab fa-twitter'>
-                  <span className='icon'>Twitter</span>
-                </li>
-              </a>
-              <a href='mailto:lisaceedesign@gmail.com?Subject=Hello'>
-                <li className='fas fa-envelope'>
-                  <span className='icon'>Mail</span>
-                </li>
-              </a>
-            </ul>
-          </div> */}
+
+          <Divider />
+
+          <a
+            href="https://www.twitter.com/lisacee14"
+            target="_"
+            rel="noopener noreferrer"
+          >
+            <ListItem button>
+              <ListItemIcon>
+                <i className="fab fa-twitter" />
+              </ListItemIcon>
+              <span class="sr-only">Twitter</span>
+            </ListItem>
+          </a>
+          <a
+            href="https://www.github.com/lisacee"
+            target="_"
+            rel="noopener noreferrer"
+          >
+            <ListItem button>
+              <ListItemIcon>
+                <i className="fab fa-github" />
+              </ListItemIcon>
+              <span class="sr-only">GitHub</span>
+            </ListItem>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/lisacee"
+            target="_"
+            rel="noopener noreferrer"
+          >
+            <ListItem button>
+              <ListItemIcon>
+                <i className="fab fa-linkedin" />
+              </ListItemIcon>
+              <span class="sr-only">LinkedIn</span>
+            </ListItem>
+          </a>
+          <a href="mailto:lisaceedesign@gmail.com?Subject=Hello">
+            <ListItem button>
+              <ListItemIcon>
+                <i className="fas fa-envelope fa-2x" />
+              </ListItemIcon>
+              <span class="sr-only">Email</span>
+            </ListItem>
+          </a>
         </List>
       </Drawer>
     </div>
